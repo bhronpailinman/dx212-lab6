f0e39adfac0f5ae771ac067ac9fae3281d80933d
bhronpailinman <bhronpailin.man@dome.tu.ac.th>
2026-09-16 11:14:24 +0700

    finist lab5

--- modern-js.js
+++ modern-js.js
@@ -11,3 +11,37 @@ const student = { name: "ฟ้า", faculty: "CITU", year: 2 };
 const { name, faculty } = student;          // ดึงค่าออกมาเป็นตัวแปร
 const updated = { ...student, year: 3 };    // copy แล้วแก้บางค่า
 console.log(name, faculty, updated);
+
+const buses = [
+  { route: "NGV-1", passengers: 45, late: false },
+  { route: "NGV-2", passengers: 62, late: true },
+  { route: "NGV-3", passengers: 38, late: true },
+  { route: "NGV-4", passengers: 51, late: false },
+];
+for(let i = 0; i < buses.length; i++) {
+    //console.log(buses[i].route, buses[i].passengers, buses[i].late);
+}
+
+for (const bus of buses) {
+    //console.log(bus.route, bus.passengers, bus.late);
+}
+
+const routes = buses.map(bus => bus.route);
+//console.log(routes);
+
+const lateBuses = buses.filter(({late}) => late==true);
+const HeavyBuses = buses.filter(({passengers}) => passengers>50);
+
+console.log(lateBuses);
+console.log(HeavyBuses);
+
+const totalPassengers = buses.reduce(
+    (total, {passengers}) => total + passengers, 0
+);
+
+const totalPassengersOfHeavyBuses = buses.filter(({passengers}) => passengers>50).reduce(
+    (total, {passengers}) => total + passengers, 0
+);
+
+console.log(totalPassengers);
+console.log(totalPassengersOfHeavyBuses);
